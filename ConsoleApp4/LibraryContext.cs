85aa9616acd471da165f84f5cd000b07e923ad74
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-10-01 11:29:07 +0330

    Add project files.

---
+++ ConsoleApp4/LibraryContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp4
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=root;password=;sslmode = none");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
